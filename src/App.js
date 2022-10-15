f5c4b25a40f33fa38acdc73e0aa44fa9f40ae29a
gradobyk <103375580+gradobyk@users.noreply.github.com>
2022-10-15 17:20:44 +0300

    jhjhjh

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-<h1>Привіт тупенька</h1>
+      <h1>Привіт тупенька</h1>
+      <footer>
+        This project was coded by Yuliya Gradobyk and is{" "}
+        <a href="https://github.com/gradobyk/react-too" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
